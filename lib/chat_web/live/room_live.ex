e4e0858cc0f76f8961c324a3ea3d0b6e7c36a20c
Ross Birdsall <ross.birdsall9@gmail.com>
2022-04-11 11:25:37 -0400

    feat: render messages performantly

--- lib/chat_web/live/room_live.ex
+++ lib/chat_web/live/room_live.ex
@@ -4,6 +4,27 @@ defmodule ChatWeb.RoomLive do
 
   @impl true
   def mount(%{"id" => room_id}, _session, socket) do
-    {:ok, assign(socket, room_id: room_id)}
+    topic = "room:" <> room_id
+    ChatWeb.Endpoint.subscribe(topic)
+
+    {:ok,
+     assign(socket,
+       room_id: room_id,
+       topic: topic,
+       messages: [%{uuid: UUID.uuid4(), content: "Hello World!"}],
+       temporary_assigns: [messages: []]
+     )}
+  end
+
+  @impl true
+  def handle_event("submit_message", %{"chat" => %{"message" => message}}, socket) do
+    message = %{uuid: UUID.uuid4(), content: message}
+    ChatWeb.Endpoint.broadcast(socket.assigns.topic, "new-message", message)
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_info(%{event: "new-message", payload: message}, socket) do
+    {:noreply, assign(socket, messages: [message])}
   end
 end
--- mix.exs
+++ mix.exs
@@ -47,7 +47,8 @@ defmodule Chat.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:mnemonic_slugs, "~> 0.0.3"},
-      {:dart_sass, "~> 0.4", runtime: Mix.env() == :dev}
+      {:dart_sass, "~> 0.4", runtime: Mix.env() == :dev},
+      { :uuid, "~> 1.1" }
     ]
   end
 
